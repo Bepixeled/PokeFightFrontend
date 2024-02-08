64d919f78f87dd9e3f25a3fd0698e2248ef6d08d
kenkindermann <ken.kindermann@gmail.com>
2024-02-08 17:28:18 +0100

    add pokecards

--- src/components/ChoosePokemon.jsx
+++ src/components/ChoosePokemon.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import PokemonCard from "./PokemonCard";
+import axios from "axios";
 
 const ChoosePokemon = () => {
+  const [allPokemons, setAllPokemons] = useState([]);
+
   const getPokemons = async () => {
     try {
-      const response = await axios.get();
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+      setAllPokemons(response.data.results);
     } catch (error) {}
   };
 
+  getPokemons();
+
   return (
-    <section className="bg-[url('/src/assets/images/background.jpg')] min-w-full min-h-screen mx-auto bg-cover bg-no-repeat p-4 flex flex-col items-center justify-center">
-      <h1 className="font-pokemon font-bold text-yellow-400 text-4xl">Choose your Pokemon</h1>
-      <input type="text" className="mt-8 rounded bg-white opacity-65 p-2" placeholder="Search..." />
-      <PokemonCard />
+    <section className="bg-[url('/src/assets/images/background.jpg')] min-w-full min-h-screen mx-auto bg-cover bg-no-repeat p-4 flex flex-col items-center">
+      <h1 className="text-4xl lg:text-5xl font-bold text-yellow-300 font-outline-2 font-pokemon mt-24">
+        Choose your Pokemon
+      </h1>
+      <input type="text" className="mt-8 rounded bg-white opacity-65 p-2 w-64" placeholder="Search..." />
+      <div className="grid grid-cols-4 mt-8 gap-8">
+        {allPokemons.map((pokemon, index) => (index < 8 ? <PokemonCard /> : null))}
+      </div>
     </section>
   );
 };
