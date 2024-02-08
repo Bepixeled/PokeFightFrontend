52217d8e45e11a6d7539b33e6a56d7d0638e3b88
kenkindermann <ken.kindermann@gmail.com>
2024-02-08 17:30:24 +0100

    change props to data prop

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const CustomCard = ({ name, imageUrl, hp, attack, defense, speed }) => {
-   return (
+const CustomCard = ({ data }) => {
+  return (
     <div className="flex flex-col justify-center items-center w-72 lg:w-96 h-96 lg:h-[80%] bg-card-bg-left bg-cover bg-center rounded-xl border-white border-solid border-8">
-      <h2 className="font-pokemon font-bold  text-1xl lg:text-4xl font-outline-2 text-yellow-300 ">{name}</h2>
+      <h2 className="font-pokemon font-bold  text-1xl lg:text-4xl font-outline-2 text-yellow-300 ">{data.name}</h2>
       <p></p>
-      <img src={imageUrl} alt="" className="w-40 lg:w-80 lg:h-80 rounded-lg my-4"  />
+      <img src={data.imageUrl} alt="" className="w-40 lg:w-80 lg:h-80 rounded-lg my-4" />
       <div className="bg-white w-[50%] h-auto bg-opacity-60 p-4 rounded-lg mb-4">
         <p>
           <span className="mr-2 font-semibold">HP:</span>
-          {hp}
+          {data.hp}
         </p>
         <p>
           <span className="mr-2 font-semibold">Attack:</span>
-          {attack}
+          {data.attack}
         </p>
         <p>
           <span className="mr-2 font-semibold">Defense:</span>
-          {defense}
+          {data.defense}
         </p>
         <p>
           <span className="mr-2 font-semibold">Speed:</span>
-          {speed}
+          {data.speed}
         </p>
       </div>
     </div>
