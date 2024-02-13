54e5e5342929abaa2683f077f2db5a0f644f4e0e
Bepixeled <git@bepixeled.dev>
2024-02-13 16:26:14 +0100

    usecontext in fight, idea for fightlogic. who wants to implement it?

--- src/components/FightResult.jsx
+++ src/components/FightResult.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const FightResult = ({ winner }) => (
   <div className="text-center font-roboto bg-poke-bg bg-no-repeat bg-cover">
-    <div className="flex justify-center items-center h-screen ">      
+    <div className="flex justify-center items-center h-screen flex-col space-y-8">      
         <h1 className="text-4xl lg:text-6xl font-bold text-yellow-300 font-outline-2 font-pokemon ">
           {winner ? `${winner} wins!` : "Fight"}
-        </h1>      
+        </h1>
+        <a href="/"><div className="bg-fight-btn bg-contain h-48 w-48">
+      <button className="h-48 w-48" >
+        Fight again
+        
+      </button>
+    </div></a>     
     </div>
   </div>
 );
