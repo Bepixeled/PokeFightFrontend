2c5e701de7b7ab2416509458aed8b55e141db78c
kenkindermann <ken.kindermann@gmail.com>
2024-02-13 17:21:18 +0100

    implement loading animation, add previos and next buttons for loading other pokemons

--- src/components/ChoosePokemon.jsx
+++ src/components/ChoosePokemon.jsx
@@ -1,28 +1,34 @@
-import { useContext, useEffect, useState } from "react";
-import PokemonCard from "./PokemonCard";
-import axios from "axios";
+import { useEffect, useState } from "react";
 import CustomCard from "./CustomCard";
 import useAxios from "../hooks/useAxios";
-import { PokemonContext } from "../provider/PokemonContext";
 
 const ChoosePokemon = () => {
   const [pokemonData, setPokemonData] = useState([]);
+  const [limit, setLimit] = useState(8);
+  const [offset, setOffset] = useState(0);
   const { getData } = useAxios();
 
-  const url = "https://pokefightapi-bq3z.onrender.com/pokemon?limit=8&offset=0";
+  const url = `https://pokefightapi-bq3z.onrender.com/pokemon?limit=${limit}&offset=${offset}`;
+
+  const fetchPokemons = async () => {
+    const allPokemons = await getData(url);
+    setPokemonData(allPokemons);
+  };
 
   useEffect(() => {
-    const fetchAllPokemons = async () => {
-      const allPokemons = await getData(url);
-      setPokemonData(allPokemons);
-    };
+    fetchPokemons();
+  }, [offset]);
 
-    fetchAllPokemons();
-  }, []);
+  const changeOffset = (value) => {
+    if (offset === 0 && value < 0) {
+      fetchPokemons();
+      return;
+    }
+    const newOffset = offset + value;
 
-  useEffect(() => {
-    console.log(pokemonData);
-  }, [pokemonData]);
+    console.log(newOffset);
+    setOffset(newOffset);
+  };
 
   return (
     <section className="bg-[url('/src/assets/images/background.jpg')] min-w-full min-h-screen mx-auto bg-cover bg-no-repeat p-4 flex flex-col items-center">
@@ -31,7 +37,27 @@ const ChoosePokemon = () => {
       </h1>
       <input type="text" className="mt-8 rounded bg-white opacity-65 p-2 w-64 mb-12" placeholder="Search..." />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mt-8 gap-8">
-        {pokemonData.map((pokemon, index) => (index < 8 ? <CustomCard key={pokemon.name} data={pokemon} /> : null))}
+        {[...Array(8)].map((_, index) => (
+          <CustomCard key={index} data={pokemonData ? pokemonData[index] : undefined} />
+        ))}
+      </div>
+      <div className="flex gap-16 text-2xl lg:text-3xl font-bold text-yellow-300 font-outline-2 font-pokemon">
+        <span
+          className="cursor-pointer"
+          onClick={() => {
+            setPokemonData(null), changeOffset(-8);
+          }}
+        >
+          Previous
+        </span>
+        <span
+          className="cursor-pointer"
+          onClick={() => {
+            setPokemonData(null), changeOffset(8);
+          }}
+        >
+          Next
+        </span>
       </div>
     </section>
   );
--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -8,39 +8,49 @@ const CustomCard = ({ data }) => {
   const navigate = useNavigate();
 
   return (
-    data && (
-      <div
-        onClick={() => {
-          setCurrentPokemon(data), navigate("/fight");
-        }}
-      >
-        <FlippingCard>
-          <FlippingCardFront>
-            <div className="flex flex-col justify-center items-center w-60 h-96 h-80 bg-card-bg-left bg-cover bg-center rounded-xl border-white border-solid border-8">
-              <h2 className="font-pokemon font-bold text-4xl font-outline-2 text-yellow-300 ">{data.name}</h2>
-              <p></p>
-              <img src={data.image} alt="" className="w-40 h-40 rounded-lg my-4" />
-            </div>
-          </FlippingCardFront>
-          <FlippingCardBack>
-            <div className="flex flex-col justify-center items-center w-60 h-80 bg-card-bg-left bg-cover bg-center rounded-xl border-white border-solid border-8">
-              <p>
-                <span className="mr-2 font-semibold">HP:</span>
-                {data.hp}
-              </p>
-              <p>
-                <span className="mr-2 font-semibold">Attack:</span>
-                {data.attack}
-              </p>
-              <p>
-                <span className="mr-2 font-semibold">Defense:</span>
-                {data.defense}
-              </p>
-            </div>
-          </FlippingCardBack>
-        </FlippingCard>
-      </div>
-    )
+    <div
+      onClick={() => {
+        setCurrentPokemon(data), navigate("/fight");
+      }}
+    >
+      <FlippingCard>
+        <FlippingCardFront>
+          <div
+            className={`flex flex-col justify-center items-center w-60 h-96 h-80 bg-card-bg-left bg-cover bg-center rounded-xl border-white border-solid border-8 ${
+              !data ? `animate-pulse` : null
+            }`}
+          >
+            <h2 className="font-pokemon font-bold text-4xl font-outline-2 text-yellow-300 ">{data?.name}</h2>
+            <p></p>
+            <img src={data?.image} alt="" className="w-40 h-40 rounded-lg my-4" />
+          </div>
+        </FlippingCardFront>
+        <FlippingCardBack>
+          <div className="flex flex-col justify-center items-center w-60 h-80 bg-card-bg-left bg-cover bg-center rounded-xl border-white border-solid border-8">
+            <p>
+              <span className="mr-2 font-semibold">HP:</span>
+              {data?.hp}
+            </p>
+            <p>
+              <span className="mr-2 font-semibold">Attack:</span>
+              {data?.attack}
+            </p>
+            <p>
+              <span className="mr-2 font-semibold">Defense:</span>
+              {data?.defense}
+            </p>
+            <p>
+              <span className="mr-2 font-semibold">Speed:</span>
+              {data?.speed}
+            </p>
+            <p>
+              <span className="mr-2 font-semibold">Type:</span>
+              {data?.type}
+            </p>
+          </div>
+        </FlippingCardBack>
+      </FlippingCard>
+    </div>
   );
 };
 
